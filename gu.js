cc22f5d9c4144b81fda05b29b9c77a0b6c4e5114
Vener <example@gmail.com>
2021-01-16 14:09:00 +0500

    first commit

---
+++ gu.js
@@ -0,0 +1,15 @@
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr,'\n', decr,'\n');
+
+
+
+
+const isChecked = true,
+      isClose   = true;
+
+
+console.log((isChecked && isClose, isClose||isChecked));
